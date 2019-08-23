4f58254e1bf750c5723d6655ce2313004ad56b5c
Chris Muga <ChrisMuga94@gmail.com>
2019-08-23 15:45:53 +0300

    initial commit

---
+++ src/components/Dashboard/MainPage.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MainPage(props){
+    return (
+    <div className = "container-fluid">
+        <div className ="row">
+            <div className = "col-md-6">
+                <div className = "alert alert-success">
+                    <h1>Dashboard</h1>
+                </div>
+            </div>
+        </div>
+        
+    </div>)
+}
+
+export default MainPage
