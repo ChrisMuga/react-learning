c33ea85dcff448770e54c3098d7a1686d0670957
Chris Muga <ChrisMuga94@gmail.com>
2019-08-23 16:20:04 +0300

    create menu that fetches from list and passes down props to children

--- src/components/Dashboard/MainPage.jsx
+++ src/components/Dashboard/MainPage.jsx
@@ -1,20 +1,50 @@
 import React from "react"
+import { menuItems } from "./data"
 
 function MainPage(props) {
 	return (
 		<div className="container-fluid">
-			<Title name="I do the Dash" />
+			{/* pass down value through props */}
+			<Title name="Dashboard" />
+			<div className="row my-1">
+				<Menu title="Menu" items={menuItems} />
+			</div>
 		</div>
 	)
 }
 
 function Title(props) {
+	const { name } = props
 	return (
 		<div className="row d-flex flex-column my-1">
 			<div className="align-self-center col-md-6">
 				<div className="alert alert-success">
-					<h1>{props.name}</h1>
+					<h1>{name}</h1>
 				</div>
+			</div>
+		</div>
+	)
+}
+
+function Menu(props) {
+	const { items, title } = props
+	let itemsOrNone
+	if (items) {
+		itemsOrNone = items.map(item => {
+			return (
+				<a href="/" className="list-group-item list-group-item-action" key={item.id}>
+					{item.title}
+				</a>
+			)
+		})
+	} else {
+		itemsOrNone = <h1>Nothing Here</h1>
+	}
+	return (
+		<div className="col-md-4">
+			<div className="list-group">
+				<div className="list-group-item active">{title}</div>
+				{itemsOrNone}
 			</div>
 		</div>
 	)
---
+++ src/components/Dashboard/data.js
@@ -0,0 +1,12 @@
+const menuItems = [
+	{
+		id: 1,
+		title: "Clock"
+	},
+	{
+		id: 2,
+		title: "Watch"
+	}
+]
+
+export { menuItems }
