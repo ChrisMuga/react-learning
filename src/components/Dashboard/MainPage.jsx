1fd56ca5a8d7d3abe508cd0e0209c3622250847b
Chris Muga <ChrisMuga94@gmail.com>
2019-08-23 15:50:58 +0300

    pass down values with props

--- src/components/Dashboard/MainPage.jsx
+++ src/components/Dashboard/MainPage.jsx
@@ -1,17 +1,23 @@
 import React from "react"
 
-function MainPage(props){
-    return (
-    <div className = "container-fluid">
-        <div className ="row">
-            <div className = "col-md-6">
-                <div className = "alert alert-success">
-                    <h1>Dashboard</h1>
-                </div>
-            </div>
-        </div>
-        
-    </div>)
+function MainPage(props) {
+	return (
+		<div className="container-fluid">
+			<Title name="I do the Dash" />
+		</div>
+	)
+}
+
+function Title(props) {
+	return (
+		<div className="row d-flex flex-column my-1">
+			<div className="align-self-center col-md-6">
+				<div className="alert alert-success">
+					<h1>{props.name}</h1>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default MainPage
