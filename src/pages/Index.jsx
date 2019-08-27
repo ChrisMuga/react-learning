0c7d53b2e432deef19122005712ea5871d9467a5
Chris Muga <ChrisMuga94@gmail.com>
2019-08-27 09:41:24 +0300

    start with user component

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,7 +1,21 @@
 import React from "react"
 import Title from "./Title"
+import Users from "./Users"
 function Index(props) {
-	return <Title name="Index" />
+	return (
+		<div>
+			<div className="row d-flex justify-content-center">
+				<div className="col-md-6">
+					<Title name="Index" />
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-md-6">
+					<Users />
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Index
